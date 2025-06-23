758d34750eabf367970a8a192843599ce853ed3a
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-06-23 18:51:56 +0200

    fix query for filter check

--- small_queries.sql
+++ small_queries.sql
@@ -41,6 +41,6 @@ SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_
 SELECT table_3.id, table_3.col_3, table_4.id, table_4.col_4 FROM table_3, table_4 WHERE table_3.id = table_4.id;
 
 -- Strict Filter comparison test
-SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id WHERE table_1.col_1 < 500;
+SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id AND table_1.col_1 < 500;
 SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id;
 

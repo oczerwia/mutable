8cebdab02f18f1eaa71767b1b23008f7d0ab4090
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-07-01 22:04:38 +0200

    adjust formatiing

--- zzz_micro_query.sql
+++ zzz_micro_query.sql
@@ -1,3 +1,24 @@
+CREATE DATABASE job_light;
+USE job_light;
+
+CREATE TABLE table_1 (
+    id INT(4) NOT NULL PRIMARY KEY,
+    col_1 INT(4) NOT NULL
+);
+IMPORT INTO table_1 DSV "benchmark/small/data/table1.csv";
+CREATE TABLE table_2 (
+    id INT(4) NOT NULL PRIMARY KEY,
+    col_2 INT(4) NOT NULL
+);
+IMPORT INTO table_2 DSV "benchmark/small/data/table2.csv";
+CREATE TABLE table_3 (
+    id INT(4) NOT NULL PRIMARY KEY,
+    col_3 INT(4) NOT NULL
+);
+IMPORT INTO table_3 DSV "benchmark/small/data/table3.csv";
+
+SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id AND table_1.col_1 < 400;
+
 SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id;
 
 

9e7ff0b5e377ca5fa1ecdf3df01758dca17e4bd2
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-07-20 17:10:51 +0200

    update statistisc generation code placement

--- src/catalog/DatabaseCommand.cpp
+++ src/catalog/DatabaseCommand.cpp
@@ -9,12 +9,9 @@
 #include <mutable/storage/Index.hpp>
 #include <mutable/util/DotTool.hpp>
 
-
 using namespace m;
 
-
 void EmptyCommand::execute(Diagnostic &diag) { /* Nothing to be done. */ }
-
 
 /*======================================================================================================================
  * Instructions
@@ -23,21 +20,31 @@ void EmptyCommand::execute(Diagnostic &diag) { /* Nothing to be done. */ }
 void learn_spns::execute(Diagnostic &diag)
 {
     auto &C = Catalog::Get();
-    if (not C.has_database_in_use()) { diag.err() << "No database selected.\n"; return; }
+    if (not C.has_database_in_use())
+    {
+        diag.err() << "No database selected.\n";
+        return;
+    }
 
     auto &DB = C.get_database_in_use();
-    if (DB.size() == 0) { diag.err() << "There are no tables in the database.\n"; return; }
+    if (DB.size() == 0)
+    {
+        diag.err() << "There are no tables in the database.\n";
+        return;
+    }
 
     auto CE = C.create_cardinality_estimator(C.pool("Spn"), DB.name);
     auto spn_estimator = cast<SpnEstimator>(CE.get());
     spn_estimator->learn_spns();
     DB.cardinality_estimator(std::move(CE));
 
-    if (not Options::Get().quiet) { diag.out() << "Learned SPN on every table in " << DB.name << ".\n"; }
+    if (not Options::Get().quiet)
+    {
+        diag.out() << "Learned SPN on every table in " << DB.name << ".\n";
+    }
 }
 
-__attribute__((constructor(201)))
-static void register_instructions()
+__attribute__((constructor(201))) static void register_instructions()
 {
     Catalog &C = Catalog::Get();
 #define REGISTER(NAME, DESCRIPTION) \
@@ -54,10 +61,12 @@ void QueryDatabase::execute(Diagnostic &diag)
 {
     Catalog &C = Catalog::Get();
 
-    if (auto stmt = cast<ast::Stmt>(&ast())) {
+    if (auto stmt = cast<ast::Stmt>(&ast()))
+    {
         if (Options::Get().ast)
             stmt->dump(diag.out());
-        if (Options::Get().astdot) {
+        if (Options::Get().astdot)
+        {
             DotTool dot(diag);
             stmt->dot(dot.stream());
             dot.show("ast", false, "dot");
@@ -73,12 +82,14 @@ void QueryDatabase::execute(Diagnostic &diag)
 
     if (Options::Get().graph)
         graph_->dump(std::cout);
-    if (Options::Get().graphdot) {
+    if (Options::Get().graphdot)
+    {
         DotTool dot(diag);
         graph_->dot(dot.stream());
         dot.show("graph", false, "fdp");
     }
-    if (Options::Get().graph2sql) {
+    if (Options::Get().graph2sql)
+    {
         graph_->sql(std::cout);
         std::cout.flush();
     }
@@ -93,7 +104,8 @@ void QueryDatabase::execute(Diagnostic &diag)
 
     if (Options::Get().plan)
         producer->dump(diag.out());
-    if (Options::Get().plandot) {
+    if (Options::Get().plandot)
+    {
         DotTool dot(diag);
         producer->dot(dot.stream());
         dot.show("logical_plan", false, "dot");
@@ -125,7 +137,7 @@ void QueryDatabase::execute(Diagnostic &diag)
         M_TIME_EXPR(backend->execute(*physical_plan_), "Execute query", C.timer());
 }
 
-void InsertRecords::execute(Diagnostic&)
+void InsertRecords::execute(Diagnostic &)
 {
     Catalog &C = Catalog::Get();
     auto &DB = C.get_database_in_use();
@@ -139,41 +151,47 @@ void InsertRecords::execute(Diagnostic&)
 
     /* Find timestamp attributes */
     auto ts_begin = std::find_if(T.cbegin_hidden(), T.end_hidden(),
-                                 [&](const Attribute & attr) {
-                                    return attr.name == C.pool("$ts_begin");
-    });
+                                 [&](const Attribute &attr)
+                                 {
+                                     return attr.name == C.pool("$ts_begin");
+                                 });
     auto ts_end = std::find_if(T.cbegin_hidden(), T.end_hidden(),
-                               [&](const Attribute & attr) {
-                                    return attr.name == C.pool("$ts_end");
-    });
+                               [&](const Attribute &attr)
+                               {
+                                   return attr.name == C.pool("$ts_end");
+                               });
 
     /* Write all tuples to the store. */
-    for (auto &t : I.tuples) {
+    for (auto &t : I.tuples)
+    {
         StackMachine get_tuple(Schema{});
-        for (std::size_t i = 0; i != t.size(); ++i) {
+        for (std::size_t i = 0; i != t.size(); ++i)
+        {
             auto attr_id = T.convert_id(i); // hidden attributes change the actual id of the attribute
             auto &v = t[i];
-            switch (v.first) {
-                case ast::InsertStmt::I_Null:
-                    get_tuple.emit_St_Tup_Null(0, i);
-                    break;
+            switch (v.first)
+            {
+            case ast::InsertStmt::I_Null:
+                get_tuple.emit_St_Tup_Null(0, i);
+                break;
 
-                case ast::InsertStmt::I_Default:
-                    /* nothing to be done, Tuples are initialized to default values */
-                    break;
+            case ast::InsertStmt::I_Default:
+                /* nothing to be done, Tuples are initialized to default values */
+                break;
 
-                case ast::InsertStmt::I_Expr:
-                    get_tuple.emit(*v.second);
-                    get_tuple.emit_Cast(S[attr_id].type, v.second->type());
-                    get_tuple.emit_St_Tup(0, attr_id, S[attr_id].type);
-                    break;
+            case ast::InsertStmt::I_Expr:
+                get_tuple.emit(*v.second);
+                get_tuple.emit_Cast(S[attr_id].type, v.second->type());
+                get_tuple.emit_St_Tup(0, attr_id, S[attr_id].type);
+                break;
             }
         }
-        Tuple *args[] = { &tup };
+        Tuple *args[] = {&tup};
         get_tuple(args);
 
         /*----- set timestamps if available. -----*/
-        if (ts_begin != T.end_hidden()) {
+        if (ts_begin != T.end_hidden())
+        {
             tup.set(ts_begin->id, Value(transaction()->start_time()));
             /* Set $ts_end to -1. It is a special value representing infinity. */
             M_insist(ts_end != T.end_hidden());
@@ -186,12 +204,12 @@ void InsertRecords::execute(Diagnostic&)
     DB.invalidate_indexes(T.name());
 }
 
-void UpdateRecords::execute(Diagnostic&)
+void UpdateRecords::execute(Diagnostic &)
 {
     M_unreachable("not yet implemented");
 }
 
-void DeleteRecords::execute(Diagnostic&)
+void DeleteRecords::execute(Diagnostic &)
 {
     M_unreachable("not yet implemented");
 }
@@ -199,25 +217,38 @@ void DeleteRecords::execute(Diagnostic&)
 void ImportDSV::execute(Diagnostic &diag)
 {
     Catalog &C = Catalog::Get();
-    try {
+    try
+    {
         DSVReader R(table_, cfg_, diag, transaction());
 
         errno = 0;
         std::ifstream file(path_);
-        if (not file) {
+        if (not file)
+        {
             const auto errsv = errno;
             diag.err() << "Could not open file " << path_;
             if (errsv)
                 diag.err() << ": " << strerror(errsv);
             diag.err() << std::endl;
-        } else {
-            M_TIME_EXPR(R(file, path_.c_str()), "Read DSV file", C.timer());
         }
-    } catch (m::invalid_argument e) {
+        else
+        {
+            M_TIME_EXPR(R(file, path_.c_str()), "Read DSV file", C.timer());
+            auto *stats = const_cast<TableStatistics*>(table_.statistics());
+            if (stats) {
+                std::cout << "Computing statistics for table: " << *table_.name() << std::endl;
+                stats->compute(table_);
+                std::cout << "Statistics computed in DatabaseCommand.cpp. Found " << stats->selectivity.size() << " columns." << std::endl;
+            } else {
+                std::cout << "ERROR: Could not create/access statistics for table!" << std::endl;
+            }
+        }
+    }
+    catch (m::invalid_argument e)
+    {
         diag.err() << "Error reading DSV file: " << e.what() << "\n";
     }
 }
-
 
 /*======================================================================================================================
  * Data Definition Language
@@ -225,22 +256,28 @@ void ImportDSV::execute(Diagnostic &diag)
 
 void CreateDatabase::execute(Diagnostic &diag)
 {
-    try {
+    try
+    {
         Catalog::Get().add_database(db_name_);
         if (not Options::Get().quiet)
             diag.out() << "Created database " << db_name_ << ".\n";
-    } catch (std::invalid_argument) {
+    }
+    catch (std::invalid_argument)
+    {
         diag.err() << "Database " << db_name_ << " already exists.\n";
     }
 }
 
 void DropDatabase::execute(Diagnostic &diag)
 {
-    try {
+    try
+    {
         Catalog::Get().drop_database(db_name_);
         if (not Options::Get().quiet)
             diag.out() << "Dropped database " << db_name_ << ".\n";
-    } catch (std::invalid_argument) {
+    }
+    catch (std::invalid_argument)
+    {
         diag.err() << "Database " << db_name_ << " does not exist.\n";
     }
 }
@@ -248,12 +285,15 @@ void DropDatabase::execute(Diagnostic &diag)
 void UseDatabase::execute(Diagnostic &diag)
 {
     auto &C = Catalog::Get();
-    try {
+    try
+    {
         auto &DB = C.get_database(db_name_);
         C.set_database_in_use(DB);
         if (not Options::Get().quiet)
             diag.out() << "Using database " << db_name_ << ".\n";
-    } catch (std::out_of_range) {
+    }
+    catch (std::out_of_range)
+    {
         diag.err() << "Database " << db_name_ << " does not exist.\n";
     }
 }
@@ -264,9 +304,12 @@ void CreateTable::execute(Diagnostic &diag)
     auto &DB = C.get_database_in_use();
     ThreadSafePooledString table_name = table_->name();
     Table *table = nullptr;
-    try {
+    try
+    {
         table = &DB.add(std::move(table_));
-    } catch (std::invalid_argument) {
+    }
+    catch (std::invalid_argument)
+    {
         diag.err() << "Table " << table_name << " already exists in database " << DB.name << ".\n";
     }
 
@@ -282,12 +325,16 @@ void DropTable::execute(Diagnostic &diag)
     auto &C = Catalog::Get();
     auto &DB = C.get_database_in_use();
 
-    for (auto &table_name : table_names_) {
-        try {
+    for (auto &table_name : table_names_)
+    {
+        try
+        {
             DB.drop_table(table_name);
             if (not Options::Get().quiet)
                 diag.out() << "Dropped table " << table_name << ".\n";
-        } catch (std::invalid_argument) {
+        }
+        catch (std::invalid_argument)
+        {
             diag.err() << "Table " << table_name << " does not exist in Database " << DB.name << ".\n";
         }
     }
@@ -301,29 +348,39 @@ void CreateIndex::execute(Diagnostic &diag)
 
     /* Compute bulkloading schema from attribute name. */
     Schema schema;
-    for (auto &entry : table.schema()) {
-        if (entry.id.name == attribute_name_) {
+    for (auto &entry : table.schema())
+    {
+        if (entry.id.name == attribute_name_)
+        {
             schema.add(entry);
             break; // only one-dimensional indexes are supported
         }
     }
 
     /* Bulkload index. */
-    try {
+    try
+    {
         M_TIME_EXPR(index_->bulkload(table, schema), "Bulkload index", C.timer());
-    } catch (invalid_argument) {
+    }
+    catch (invalid_argument)
+    {
         diag.err() << "Could not bulkload index." << '\n';
     }
 
     /* Add index to database. */
-    try {
+    try
+    {
         DB.add_index(std::move(index_), table_name_, attribute_name_, index_name_);
         if (not Options::Get().quiet)
             diag.out() << "Created index " << index_name_ << ".\n";
-    } catch (std::out_of_range) {
+    }
+    catch (std::out_of_range)
+    {
         diag.err() << "Table " << table_name_ << " or Attribute " << attribute_name_ << " does not exist in Database "
                    << DB.name << ".\n";
-    } catch (invalid_argument) {
+    }
+    catch (invalid_argument)
+    {
         diag.err() << "Index " << index_name_ << " already exists in Database " << DB.name << ".\n";
     }
 }
@@ -333,18 +390,22 @@ void DropIndex::execute(Diagnostic &diag)
     auto &C = Catalog::Get();
     auto &DB = C.get_database_in_use();
 
-    for (auto &index_name : index_names_) {
-        try {
+    for (auto &index_name : index_names_)
+    {
+        try
+        {
             DB.drop_index(index_name);
             if (not Options::Get().quiet)
                 diag.out() << "Dropped index " << index_name << ".\n";
-        } catch (invalid_argument) {
+        }
+        catch (invalid_argument)
+        {
             diag.err() << "Index " << index_name << " does not exist in Database " << DB.name << ".\n";
         }
     }
 }
 
-#define ACCEPT(CLASS) \
+#define ACCEPT(CLASS)                                           \
     void CLASS::accept(DatabaseCommandVisitor &v) { v(*this); } \
     void CLASS::accept(ConstDatabaseCommandVisitor &v) const { v(*this); }
 M_DATABASE_COMMAND_LIST(ACCEPT)

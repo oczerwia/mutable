f40a3f22fb02f4e2558e25117c7d8a31ddbe4a59
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-08-06 20:55:39 +0200

    change to single test query with grouping

--- zzz_micro_query.sql
+++ zzz_micro_query.sql
@@ -17,11 +17,9 @@ CREATE TABLE table_3 (
 );
 IMPORT INTO table_3 DSV "benchmark/small/data/table3.csv";
 
-SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id AND table_1.col_1 < 400;
-
-SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id;
-
-
-SELECT table_3.id, table_3.col_3, table_1.id, table_1.col_1 FROM table_3, table_1 WHERE table_3.id = table_1.id;
-SELECT table_2.id, table_2.col_2, table_1.id, table_1.col_1 FROM table_2,  table_1 WHERE table_2.id = table_1.id ;
-SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2, table_3.id, table_3.col_3 FROM table_1, table_2, table_3 WHERE table_1.id = table_2.id AND table_1.id = table_3.id;
+SELECT COUNT(table_1.col_1) 
+FROM table_1, table_2, table_3 
+WHERE table_1.id = table_2.id 
+    AND table_1.id = table_3.id 
+    AND table_1.col_1 < 400
+GROUP BY table_1.col_1;

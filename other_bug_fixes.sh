ef36be9adec3f2e629d45bf32f26b62d87bb6b42
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-08-15 00:30:40 +0200

    add all tables that need fixing

--- other_bug_fixes.sh
+++ other_bug_fixes.sh
@@ -18,4 +18,9 @@ export CXX="ccache $(brew --prefix llvm@17)/bin/clang++"
 
 
 # Use csvclean to clean out faulty lines in job-light benchmark
+
+touch benchmark/job-ligh/data/movie_companies_cleaned.csv
+touch benchmark/job-ligh/data/movie_info_idx_cleaned.csv
+
 csvclean -a benchmark/job-light/data/movie_companies.csv >> benchmark/job-ligh/data/movie_companies_cleaned.csv
+csvclean -a benchmark/job-light/data/movie_info_idx.csv >> benchmark/job-ligh/data/movie_info_idx_cleaned.csv

531ee133469d28cf31dd5eac33baf9792f2372af
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-09-09 21:09:03 +0200

    limit to only testing experimentalrangeestimator

--- zzzz_run_estimator_benchmark.sh
+++ zzzz_run_estimator_benchmark.sh
@@ -12,77 +12,135 @@ QUERY_FILE="${PWD}/benchmark/job-light/ALL_QUERIES.sql"
 
 echo "Running all configurations from launch.json..."
 
-echo "Running CartesianProduct..."
-$PROGRAM \
-    --no-statistics \
-    --no-learn-cardinalities  \
-    --cardinality-csv ${OUTPUT_DIR}/CartesianProduct.csv \
-    --plan-enumerator GOO \
-    --cardinality-estimator CartesianProduct \
-    --backend Interpreter \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/CartesianProduct_output.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/CartesianProduct_output.log"
+# echo "Running CartesianProduct..."
+# $PROGRAM \
+#     --no-statistics \
+#     --no-learn-cardinalities  \
+#     --cardinality-csv ${OUTPUT_DIR}/CartesianProduct.csv \
+#     --plan-enumerator GOO \
+#     --cardinality-estimator CartesianProduct \
+#     --backend Interpreter \
+#     "$QUERY_FILE" > "${OUTPUT_DIR}/CartesianProduct_output.log" 2>&1
+# echo "Output saved to ${OUTPUT_DIR}/CartesianProduct_output.log"
 
-echo "Running SelectivityBased..."
-$PROGRAM \
-    --no-learn-cardinalities  \
-    --cardinality-csv ${OUTPUT_DIR}/Selecvitity.csv \
-    --plan-enumerator GOO \
-    --cardinality-estimator Selectivitybased \
-    --backend Interpreter \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/SelectivityBased_output.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/SelectivityBased_output.log"
+# echo "Running SelectivityBased..."
+# $PROGRAM \
+#     --no-learn-cardinalities  \
+#     --cardinality-csv ${OUTPUT_DIR}/Selecvitity.csv \
+#     --plan-enumerator GOO \
+#     --cardinality-estimator Selectivitybased \
+#     --backend Interpreter \
+#     "$QUERY_FILE" > "${OUTPUT_DIR}/SelectivityBased_output.log" 2>&1
+# echo "Output saved to ${OUTPUT_DIR}/SelectivityBased_output.log"
 
 echo "Running ExperimentalRangeEstimator..."
 $PROGRAM \
-    --no-learn-cardinalities  \
     --plan-enumerator RangeGOO \
     --cardinality-estimator ExperimentalRange \
     --backend Interpreter \
-    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator.csv \
-    --collapse-function UpperBound \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_output.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_output.log"
+    --sample-size 100000 \
+    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator_MEAN_0.csv \
+    --collapse-function Mean \
+    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_MEAN_0.log" 2>&1
+echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_MEAN_0.log"
 
-echo "Running RangeGOO..."
+echo "Running ExperimentalRangeEstimator..."
 $PROGRAM \
-    --no-learn-cardinalities  \
     --plan-enumerator RangeGOO \
-    --cardinality-estimator RangeCartesianProduct \
+    --cardinality-estimator ExperimentalRange \
     --backend Interpreter \
-    --cardinality-csv ${OUTPUT_DIR}/range_first_try.csv \
-    --collapse-function UpperBound \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/RangeGOO_output.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/RangeGOO_output.log"
+    --sample-size 100000 \
+    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator_MEAN_1.csv \
+    --collapse-function Mean \
+    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_MEAN_1.log" 2>&1
+echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_MEAN_1.log"
 
-echo "Running Histogram..."
+echo "Running ExperimentalRangeEstimator..."
 $PROGRAM \
-    --no-learn-cardinalities  \
-    --cardinality-csv ${OUTPUT_DIR}/histogram.csv \
-    --plan-enumerator GOO \
-    --cardinality-estimator Histogram \
+    --plan-enumerator RangeGOO \
+    --cardinality-estimator ExperimentalRange \
     --backend Interpreter \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/Histogram_output.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/Histogram_output.log"
+    --sample-size 100000 \
+    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator_MEAN_2.csv \
+    --collapse-function Mean \
+    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_MEAN_2.log" 2>&1
+echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_MEAN_2.log"
 
-echo "Running DPsizeOpt..."
-$PROGRAM \
-    --no-learn-cardinalities  \
-    --plan-enumerator DPsizeOpt \
-    --backend Interpreter \
-    --cardinality-csv ${OUTPUT_DIR}/dp_size_opt.csv \
-    --cardinality-estimator CartesianProduct \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/DPsizeOpt_output.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/DPsizeOpt_output.log"
 
-echo "Running DPsizeOpt..."
+
+echo "Running ExperimentalRangeEstimator..."
 $PROGRAM \
-    --no-learn-cardinalities  \
-    --plan-enumerator DPsizeOpt \
+    --plan-enumerator RangeGOO \
+    --cardinality-estimator ExperimentalRange \
     --backend Interpreter \
-    --cardinality-csv ${OUTPUT_DIR}/dp_size_opt.csv \
-    --cardinality-estimator Selectivitybased \
-    "$QUERY_FILE" > "${OUTPUT_DIR}/DPsizeOpt_selectivity.log" 2>&1
-echo "Output saved to ${OUTPUT_DIR}/DPsizeOpt_selectivity.log"
+    --sample-size 100000 \
+    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator_UPPER_0.csv \
+    --collapse-function upperBound \
+    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_UPPER_0.log" 2>&1
+echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_UPPER_0.log"
+
+echo "Running ExperimentalRangeEstimator..."
+$PROGRAM \
+    --plan-enumerator RangeGOO \
+    --cardinality-estimator ExperimentalRange \
+    --backend Interpreter \
+    --sample-size 100000 \
+    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator_UPPER_0.csv \
+    --collapse-function upperBound \
+    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_UPPER_1.log" 2>&1
+echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_UPPER_1.log"
+
+echo "Running ExperimentalRangeEstimator..."
+$PROGRAM \
+    --plan-enumerator RangeGOO \
+    --cardinality-estimator ExperimentalRange \
+    --backend Interpreter \
+    --sample-size 100000 \
+    --cardinality-csv ${OUTPUT_DIR}/ExperimentalRangeEstimator_UPPER_0.csv \
+    --collapse-function upperBound \
+    "$QUERY_FILE" > "${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_UPPER_2.log" 2>&1
+echo "Output saved to ${OUTPUT_DIR}/ExperimentalRangeEstimator_COLLAPSE_UPPER_2.log"
+
+
+# echo "Running RangeGOO..."
+# $PROGRAM \
+#     --no-learn-cardinalities  \
+#     --plan-enumerator RangeGOO \
+#     --cardinality-estimator RangeCartesianProduct \
+#     --backend Interpreter \
+#     --cardinality-csv ${OUTPUT_DIR}/range_first_try.csv \
+#     --collapse-function UpperBound \
+#     "$QUERY_FILE" > "${OUTPUT_DIR}/RangeGOO_output.log" 2>&1
+# echo "Output saved to ${OUTPUT_DIR}/RangeGOO_output.log"
+
+# echo "Running Histogram..."
+# $PROGRAM \
+#     --no-learn-cardinalities  \
+#     --cardinality-csv ${OUTPUT_DIR}/histogram.csv \
+#     --plan-enumerator GOO \
+#     --cardinality-estimator Histogram \
+#     --backend Interpreter \
+#     "$QUERY_FILE" > "${OUTPUT_DIR}/Histogram_output.log" 2>&1
+# echo "Output saved to ${OUTPUT_DIR}/Histogram_output.log"
+
+# echo "Running DPsizeOpt..."
+# $PROGRAM \
+#     --no-learn-cardinalities  \
+#     --plan-enumerator DPsizeOpt \
+#     --backend Interpreter \
+#     --cardinality-csv ${OUTPUT_DIR}/dp_size_opt.csv \
+#     --cardinality-estimator CartesianProduct \
+#     "$QUERY_FILE" > "${OUTPUT_DIR}/DPsizeOpt_output.log" 2>&1
+# echo "Output saved to ${OUTPUT_DIR}/DPsizeOpt_output.log"
+
+# echo "Running DPsizeOpt..."
+# $PROGRAM \
+#     --no-learn-cardinalities  \
+#     --plan-enumerator DPsizeOpt \
+#     --backend Interpreter \
+#     --cardinality-csv ${OUTPUT_DIR}/dp_size_opt.csv \
+#     --cardinality-estimator Selectivitybased \
+#     "$QUERY_FILE" > "${OUTPUT_DIR}/DPsizeOpt_selectivity.log" 2>&1
+# echo "Output saved to ${OUTPUT_DIR}/DPsizeOpt_selectivity.log"
 
 echo "All configurations completed. Check the ${OUTPUT_DIR} directory for output logs."

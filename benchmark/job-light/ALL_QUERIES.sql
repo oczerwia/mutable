823c1a5b6c306548838785a4043de68e32bb2e6b
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-09-09 23:16:48 +0200

    enumerate queries

--- benchmark/job-light/ALL_QUERIES.sql
+++ benchmark/job-light/ALL_QUERIES.sql
@@ -26,8 +26,6 @@ CREATE TABLE title (
     md5sum CHAR(32)
 );
 
-
-
 CREATE TABLE movie_companies (
     id INT(4) NOT NULL PRIMARY KEY,
     movie_id INT(4) NOT NULL,
@@ -35,7 +33,6 @@ CREATE TABLE movie_companies (
     company_type_id INT(4) NOT NULL,
     note CHAR(128)
 );
-
 
 CREATE TABLE movie_info_idx (
     id INT(4) NOT NULL PRIMARY KEY,
@@ -59,155 +56,150 @@ CREATE TABLE movie_info (
     note CHAR(128)
 );
 
-
-
--- Import data from CSV files
 IMPORT INTO cast_info DSV "benchmark/job-light/data/cast_info.csv" ROWS 1000000; -- 10 Mil
 IMPORT INTO title DSV "benchmark/job-light/data/title.csv" ROWS 1000000; 
 IMPORT INTO movie_companies DSV "benchmark/job-light/data/movie_companies_cleaned.csv" ROWS 1000000; 
-IMPORT INTO movie_info_idx DSV "benchmark/job-light/data/movie_info_idx_cleaned.csv" ROWS 1000000; 
-IMPORT INTO movie_keyword DSV "benchmark/job-light/data/movie_keyword.csv" ROWS 1000000; 
+IMPORT INTO movie_info_idx DSV "benchmark/job-light/data/movie_info_idx_cleaned.csv" ROWS 1000000; -- Does not have 10 Mil
+IMPORT INTO movie_keyword DSV "benchmark/job-light/data/movie_keyword.csv" ROWS 1000000; -- Does not have 10 Mil
 IMPORT INTO movie_info DSV "benchmark/job-light/data/movie_info.csv" ROWS 1000000; 
 
-
-
-
+-- 1
 SELECT COUNT(*) FROM movie_companies, title, movie_info_idx WHERE title.id=movie_companies.movie_id AND title.id=movie_info_idx.movie_id AND movie_info_idx.info_type_id=112 AND movie_companies.company_type_id=2;
-
+-- 2
 SELECT COUNT(*) FROM movie_companies, title, movie_info_idx WHERE title.id=movie_companies.movie_id AND title.id=movie_info_idx.movie_id AND movie_info_idx.info_type_id=113 AND movie_companies.company_type_id=2 AND title.production_year>2005 AND title.production_year<2010;
-
+-- 3
 SELECT COUNT(*) FROM movie_companies, title, movie_info_idx WHERE title.id=movie_companies.movie_id AND title.id=movie_info_idx.movie_id AND movie_info_idx.info_type_id=112 AND movie_companies.company_type_id=2 AND title.production_year>2010;
-
+-- 4
 SELECT COUNT(*) FROM movie_companies, title, movie_info_idx WHERE title.id=movie_companies.movie_id AND title.id=movie_info_idx.movie_id AND movie_info_idx.info_type_id=113 AND movie_companies.company_type_id=2 AND title.production_year>2000;
-
+-- 5
 SELECT COUNT(*) FROM movie_companies, title, movie_keyword WHERE title.id=movie_companies.movie_id AND title.id=movie_keyword.movie_id AND movie_keyword.keyword_id=117;
-
+-- 6
 SELECT COUNT(*) FROM title, movie_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.production_year>2005;
-
+-- 7
 SELECT COUNT(*) FROM title, movie_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.production_year>2010;
-
+-- 8
 SELECT COUNT(*) FROM title, movie_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.production_year>1990;
-
+-- 9
 SELECT COUNT(*) FROM title, movie_info_idx, movie_keyword WHERE title.id=movie_info_idx.movie_id AND title.id=movie_keyword.movie_id AND title.production_year>2005 AND movie_info_idx.info_type_id=101;
-
+-- 10
 SELECT COUNT(*) FROM title, movie_info_idx, movie_keyword WHERE title.id=movie_info_idx.movie_id AND title.id=movie_keyword.movie_id AND title.production_year>2010 AND movie_info_idx.info_type_id=101;
-
+-- 11
 SELECT COUNT(*) FROM title, movie_info_idx, movie_keyword WHERE title.id=movie_info_idx.movie_id AND title.id=movie_keyword.movie_id AND title.production_year>1990 AND movie_info_idx.info_type_id=101;
-
+-- 12
 SELECT COUNT(*) FROM title, movie_info, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2005 AND movie_companies.company_type_id=2;
-
+-- 13
 SELECT COUNT(*) FROM title, movie_info, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2010 AND movie_companies.company_type_id=2;
-
+-- 14
 SELECT COUNT(*) FROM title, movie_info, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.production_year>1990 AND movie_companies.company_type_id=2;
-
+-- 15
 SELECT COUNT(*) FROM movie_keyword, title, cast_info WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>2010 AND movie_keyword.keyword_id=8200;
-
+-- 16
 SELECT COUNT(*) FROM movie_keyword, title, cast_info WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>2014;
-
+-- 17
 SELECT COUNT(*) FROM movie_keyword, title, cast_info WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>2014 AND movie_keyword.keyword_id=8200;
-
+-- 18
 SELECT COUNT(*) FROM movie_keyword, title, cast_info WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>2000 AND movie_keyword.keyword_id=8200;
-
+-- 19
 SELECT COUNT(*) FROM movie_keyword, title, cast_info WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>2000;
-
+-- 20
 SELECT COUNT(*) FROM cast_info, title WHERE title.id=cast_info.movie_id AND title.production_year>1980 AND title.production_year<1995;
-
+-- 21
 SELECT COUNT(*) FROM cast_info, title WHERE title.id=cast_info.movie_id AND title.production_year>1980 AND title.production_year<1984;
-
+-- 22
 SELECT COUNT(*) FROM cast_info, title WHERE title.id=cast_info.movie_id AND title.production_year>1980 AND title.production_year<2010;
-
+-- 23
 SELECT COUNT(*) FROM cast_info, title, movie_companies WHERE title.id=cast_info.movie_id AND title.id=movie_companies.movie_id AND cast_info.role_id=2;
-
+-- 24
 SELECT COUNT(*) FROM cast_info, title, movie_companies WHERE title.id=cast_info.movie_id AND title.id=movie_companies.movie_id AND cast_info.role_id=4;
-
+-- 25
 SELECT COUNT(*) FROM cast_info, title, movie_companies WHERE title.id=cast_info.movie_id AND title.id=movie_companies.movie_id AND cast_info.role_id=7;
-
+-- 26
 SELECT COUNT(*) FROM cast_info, title, movie_companies WHERE title.id=cast_info.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2005 AND title.production_year<2015 AND cast_info.role_id=2;
-
+-- 27
 SELECT COUNT(*) FROM cast_info, title, movie_companies WHERE title.id=cast_info.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2007 AND title.production_year<2010 AND cast_info.role_id=2;
-
+-- 28
 SELECT COUNT(*) FROM title, cast_info, movie_companies WHERE title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.production_year>2005 AND cast_info.role_id=1;
-
+-- 29
 SELECT COUNT(*) FROM title, cast_info, movie_companies WHERE title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.production_year>2010 AND cast_info.role_id=1;
-
+-- 30
 SELECT COUNT(*) FROM title, cast_info, movie_companies WHERE title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.production_year>1990;
-
+-- 31
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND movie_keyword.keyword_id=398 AND movie_companies.company_type_id=2 AND title.production_year>1950 AND title.production_year<2000;
-
+-- 32
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND movie_keyword.keyword_id=398 AND movie_companies.company_type_id=2;
-
+-- 33
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND title.production_year>1950;
-
+-- 34
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND movie_info_idx.info_type_id=101 AND movie_info.info_type_id=3 AND title.production_year>2005 AND title.production_year<2008 AND movie_companies.company_type_id=2;
-
+-- 35
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND movie_info_idx.info_type_id=113 AND movie_info.info_type_id=105;
-
+-- 36
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND movie_info_idx.info_type_id=101 AND movie_info.info_type_id=3 AND title.production_year>2000 AND title.production_year<2010 AND movie_companies.company_type_id=2;
-
+-- 37
 SELECT COUNT(*) FROM title, movie_info, movie_companies, movie_info_idx WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=movie_info_idx.movie_id AND title.kind_id=1 AND movie_companies.company_type_id=2 AND movie_info_idx.info_type_id=101 AND movie_info.info_type_id=16;
-
+-- 38
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.production_year>2010 AND title.kind_id=1 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 39
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.kind_id=1 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 40
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.production_year>2005 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 41
 SELECT COUNT(*) FROM title, movie_info, movie_companies, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND movie_info.info_type_id=16 AND title.production_year>2000;
-
+-- 42
 SELECT COUNT(*) FROM title, movie_info, movie_companies, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND movie_info.info_type_id=16 AND title.production_year>2005 AND title.production_year<2010;
-
+-- 43
 SELECT COUNT(*) FROM title, movie_info, movie_companies, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND movie_info.info_type_id=16 AND title.production_year>1990;
-
+-- 44
 SELECT COUNT(*) FROM cast_info, title, movie_keyword, movie_companies WHERE title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND movie_keyword.keyword_id=117;
-
+-- 45
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, cast_info WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=cast_info.movie_id AND movie_info.info_type_id=105 AND movie_info_idx.info_type_id=100;
-
+-- 46
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, cast_info WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=cast_info.movie_id AND movie_info.info_type_id=3 AND movie_info_idx.info_type_id=101 AND title.production_year>2008 AND title.production_year<2014;
-
+-- 47
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, cast_info WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=cast_info.movie_id AND movie_info.info_type_id=3 AND movie_info_idx.info_type_id=100;
-
+-- 48
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2005 AND title.production_year<2009;
-
+-- 49
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16;
-
+-- 50
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2000;
-
+-- 51
 SELECT COUNT(*) FROM title, cast_info, movie_keyword WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>1950 AND title.kind_id=1;
-
+-- 52
 SELECT COUNT(*) FROM title, cast_info, movie_keyword WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.production_year>2000 AND title.kind_id=1;
-
+-- 53
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies, movie_info WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND title.id=movie_info.movie_id AND movie_keyword.keyword_id=398 AND movie_companies.company_type_id=2 AND title.production_year>1950 AND title.production_year<2000;
-
+-- 54
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies, movie_info WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND title.id=movie_info.movie_id AND movie_keyword.keyword_id=398 AND movie_companies.company_type_id=2 AND title.production_year>2000 AND title.production_year<2010;
-
+-- 55
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies, movie_info WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND title.id=movie_info.movie_id AND movie_keyword.keyword_id=398 AND movie_companies.company_type_id=2 AND title.production_year>1950 AND title.production_year<2010;
-
+-- 56
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2008 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 57
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2009 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 58
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2010;
-
+-- 59
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2010 AND movie_companies.company_id=22956;
-
+-- 60
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2000;
-
+-- 61
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND movie_info.info_type_id=3 AND movie_info_idx.info_type_id=100;
-
+-- 62
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND movie_info.info_type_id=3 AND movie_info_idx.info_type_id=100 AND title.production_year>2010;
-
+-- 63
 SELECT COUNT(*) FROM title, cast_info, movie_keyword, movie_info_idx WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.id=movie_info_idx.movie_id AND title.production_year>2000 AND title.kind_id=1 AND movie_info_idx.info_type_id=101;
-
+-- 64
 SELECT COUNT(*) FROM title, cast_info, movie_keyword, movie_info_idx WHERE title.id=movie_keyword.movie_id AND title.id=cast_info.movie_id AND title.id=movie_info_idx.movie_id AND title.production_year>2005 AND title.kind_id=1 AND movie_info_idx.info_type_id=101;
-
+-- 65
 SELECT COUNT(*) FROM title, movie_keyword, movie_companies, movie_info WHERE title.id=movie_keyword.movie_id AND title.id=movie_companies.movie_id AND title.id=movie_info.movie_id AND movie_keyword.keyword_id=398 AND movie_companies.company_type_id=2 AND title.production_year=1998;
-
+-- 66
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2000 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 67
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, movie_keyword, movie_companies WHERE title.id=movie_info.movie_id AND title.id=movie_keyword.movie_id AND title.id=movie_info_idx.movie_id AND title.id=movie_companies.movie_id AND title.production_year>2005 AND movie_info.info_type_id=8 AND movie_info_idx.info_type_id=101;
-
+-- 68
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2000 AND title.production_year<2010 AND movie_keyword.keyword_id=7084;
-
+-- 69
 SELECT COUNT(*) FROM title, movie_info, movie_companies, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_companies.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND cast_info.role_id=2 AND movie_info.info_type_id=16 AND title.production_year>2000 AND title.production_year<2005 AND movie_keyword.keyword_id=7084;
-
+-- 70
 SELECT COUNT(*) FROM title, movie_info, movie_info_idx, cast_info, movie_keyword WHERE title.id=movie_info.movie_id AND title.id=movie_info_idx.movie_id AND title.id=cast_info.movie_id AND title.id=movie_keyword.movie_id AND movie_info.info_type_id=3 AND movie_info_idx.info_type_id=100 AND title.production_year>2000;
